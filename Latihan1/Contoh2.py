f73bf016b7326b0c89ccede0bb0a64834625f0a7
Putri <126861458+putrinarto@users.noreply.github.com>
2023-03-14 10:36:16 +0700

    Delete Latihan1 directory

--- Latihan1/Contoh2.py
+++
@@ -1,8 +0,0 @@
-class Mahasiswa:
-    def __init__(self, nama, npm):
-        self.nama = nama
-        self.npm = npm
-    def info(self):
-        print(f"Nama: {self.nama}\nNIM: {self.npm}")
-mahasiswaB = Mahasiswa("Putri", "210511068")
-mahasiswaB.info()
