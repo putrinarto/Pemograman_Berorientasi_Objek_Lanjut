88d8a933edfbbf2a9c32ae3b3d21cc28be49b72d
Putri <126861458+putrinarto@users.noreply.github.com>
2023-03-26 19:31:08 +0700

    Delete Pertemuan_1 directory

--- Pertemuan_1/Contoh4.py
+++
@@ -1,8 +0,0 @@
-class Buku:
-    def __init__(self, judul, penulis):
-        self.judul = judul
-        self.penulis = penulis
-    def info(self):
-        print(f"Judul: {self.judul}\nPenulis: {self.penulis}")
-bukuA = Buku("Panduan jaringan", "Putri")
-bukuA.info()
--- Pertemuan_1/Contoh5.py
+++
@@ -1,8 +0,0 @@
-class PesawatTerbang:
-    def __init__(self, maskapai, tujuan):
-            self.maskapai = maskapai
-            self.tujuan = tujuan
-    def info(self):
-        print(f"Maskapai: {self.maskapai}\nTujuan: {self.tujuan}")
-pesawatA = PesawatTerbang("Lion", "jakarta-bali")
-pesawatA.info()
